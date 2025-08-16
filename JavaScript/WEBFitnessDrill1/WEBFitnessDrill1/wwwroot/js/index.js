9eae88c9774140f946bc261308dcf978285413bf
recepyucegit <recep.yuce1789@gmail.com>
2025-08-16 20:43:52 +0300

    .
    .

---
+++ JavaScript/WEBFitnessDrill1/WEBFitnessDrill1/wwwroot/js/index.js
@@ -0,0 +1,9 @@
+﻿//Buttons
+var grs1 = document.getElementById("grs1")
+
+//Eylemler
+grs1.onclick = function () {
+    var grsPopUpShow = document.getElementById("grsPopUp")
+    grsPopUpShow.style = "display:block";
+
+}
