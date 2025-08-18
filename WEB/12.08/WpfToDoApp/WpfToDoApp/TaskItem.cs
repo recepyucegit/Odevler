bf2bce0b198287e6f6bf1e7263654ff5bb2f7b33
recepyucegit <recep.yuce1789@gmail.com>
2025-08-18 17:10:56 +0300

    .
    .

--- WEB/12.08/WpfToDoApp/WpfToDoApp/TaskItem.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public class TaskItem
-{
-    public string Content { get; set; }
-    public bool IsImportant { get; set; }
-    public bool IsDone { get; set; }
-    public DateTime StartTime { get; set; } // Görev eklenme zamanı
-}
