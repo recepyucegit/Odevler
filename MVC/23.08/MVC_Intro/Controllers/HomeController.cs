556bc72ffb8ac320a8bbfccb8ea35779182a5bc6
recepyucegit <recep.yuce1789@gmail.com>
2025-08-23 13:34:50 +0300

    .
    .

---
+++ MVC/23.08/MVC_Intro/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Intro.Models;
+
+namespace MVC_Intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region Test istekleri
+        //// Index
+        //public string  Index()
+        //{
+        //    return "Anasayfa";
+
+        //    //aslında olması gereken Index isimli bu metoda istek geldiğinde ilgili sayfanın gösterilmesidir.
+        //}
+
+        //// About
+        //public string About()
+        //{
+        //    return "Hakkımızda";
+        //}
+
+        //// Contact
+        //public string Contact()
+        //{
+        //    return "İletişim";
+        //} 
+        #endregion
+
+        //İletişim Listesi
+        public static List<Iletisim> contactList = new List<Iletisim>();
+
+        //Index Sayfası Aksiyon
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //About Sayfası Aksiyon
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        //Contact Sayfası Aksiyon
+        [HttpGet]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        //ilgili sayfadan herhangi bir bilgiyi yakalamak istediğimiz o sayfanın [HttpPost] action'nı oluşturmamız gerekmektedir.
+
+        [HttpPost]
+        public IActionResult Contact(string ad, string email, string mesaj)
+        {
+            Iletisim contact = new Iletisim
+            {
+                Name = ad,
+                Email = email,
+                Message = mesaj,
+            };
+
+            contactList.Add(contact);
+
+
+            return View();
+        }
+    }
+}
---
+++ MVC/23.08/MVC_Intro/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Intro.Models;
+
+namespace MVC_Intro.Controllers
+{
+    public class ProductController : Controller
+    {
+
+
+       static List<Product> _productList = new List<Product>();
+
+
+
+        [HttpGet]
+        //Ürün liste sayfası
+        public IActionResult Index()
+        {
+            return View(_productList);
+        }
+
+
+        [HttpGet]
+        //Ürün ekleme sayfası
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult Create(string productName, decimal unitPrice, string categoryName)
+        //{
+        //    //parametre değerleri "Product" nesnesi tarafında kullanılacak.
+        //    Product product = new Product()
+        //    {
+        //        ProductName = productName,
+        //        UnitPrice = unitPrice,
+        //        CategoryName = categoryName
+        //    };
+        //    _productList.Add(product);
+        //    return View();
+        //}
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            //parametre değerleri "Product" nesnesi tarafında kullanılacak.
+            product.ID=_productList.Count+1;
+            _productList.Add(product);
+            //return View();
+            return RedirectToAction("Index");
+        }
+    }
+}
