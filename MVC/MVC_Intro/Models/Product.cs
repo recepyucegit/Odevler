9081d41deae230c9614a3c89daec5ec216419514
recepyucegit <recep.yuce1789@gmail.com>
2025-08-23 12:42:32 +0300

    C
    C

---
+++ MVC/MVC_Intro/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC_Intro.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string ProductName { get; set; }
+        public string  UnitPrice { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
