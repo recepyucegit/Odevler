dd7758253ad386a22deeb9b42d777d25fc612be6
recepyucegit <recep.yuce1789@gmail.com>
2025-09-16 08:59:12 +0300

    .
    .

---
+++ Project/StoicWorkOut/DAL/Entities/Abstract/IEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities.Abstract
+{
+    public interface IEntity<T>
+    {
+        public int ID { get; set; }
+        // Master Id oluştur T tipinde
+        public T MasterId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+
+    }
+}
