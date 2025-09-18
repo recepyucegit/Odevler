44d0b206c675329a14b5c625f0c94e8bf96dbb70
recepyucegit <recep.yuce1789@gmail.com>
2025-09-18 18:45:13 +0300

    .
    .

--- Project/StoicWorkOut/DAL/Entities/Abstracts/BaseEntity.cs
+++ Project/StoicWorkOut/DAL/Entities/Abstracts/BaseEntity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL.Entities.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,24 @@ using System.Threading.Tasks;
 
 namespace DAL.Entities.Abstract
 {
-    public abstract class BaseEntity
+    public abstract class BaseEntity : IEntity<Guid>
+
     {
+        protected BaseEntity()
+        {
+            MasterId = Guid.NewGuid();
+            CreatedDate = DateTime.Now;
+            ComputerName = Environment.MachineName;
+            IpAddress = "192.168.1.1";
+        }
+
+        public int ID { get ; set ; }
+        public Guid MasterId { get ; set ; }
+        public DateTime CreatedDate { get ; set ; }
+        public DateTime? UpdatedDate { get ; set ; }
+        public string ComputerName { get ; set ; }
+        public string UpdatedComputerName { get ; set ; }
+        public string IpAddress { get ; set ; }
+        public DataStatus Status { get ; set ; }
     }
 }
