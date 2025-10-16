c90a479d1da95f66c0d64f16714857988fca7526
recepyucegit <recep.yuce1789@gmail.com>
2025-10-16 20:42:03 +0300

    .
    .

---
+++ Project/Wep_Api_Driller_1/API/Models/Branch.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApiCRUd.Models
+{
+    public class Branch
+    {
+        // Primary Key - Her şubenin benzersiz kimliği
+        public int BranchId { get; set; }
+        // Şube adı - NOT null olmalı
+        public string BranchName { get; set; }=string.Empty;
+        // Şehir bilgisi - NOT null olmalı
+        public string City { get; set; }=string.Empty;
+        // Telefon numarası - null olabilir
+        public string? Phone { get; set; }
+        // Şubenin aktif olup olmadığını gösterir
+        public bool IsActive { get; set; }
+        // Açılış Tarihi
+        public DateTime OpeningDate { get; set; }
+        // İlişkili çalışanlar - bir şubenin birden fazla çalışanı olabilir
+        public ICollection<Employee>? Employees { get; set; } 
+    }
+}
---
+++ Project/Wep_Api_Driller_1/API/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiCRUD.Models
+{
+    public class User
+    {
+        public  int UserId { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string UserName { get; set; }
+
+    }
+}
